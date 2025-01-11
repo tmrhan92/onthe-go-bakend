5f2aa1695021bab5cbc33b54ee5aeb8276441621
tmrhan92 <63450846+tmrhan92@users.noreply.github.com>
2025-01-11 22:40:21 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -46,30 +46,27 @@ res.status(500).send("خطأ في التسجيل");
 });
 
 // تسجيل الدخول
-// تسجيل الدخول
 router.post('/login', async (req, res) => {
-const { email, password } = req.body;
+    const { email, password } = req.body;
 
-try {
-const user = await User.findOne({ email });
+    try {
+        const user = await User.findOne({ email });
 
-if (!user) {
-return res.status(401).send("البريد الإلكتروني أو كلمة المرور غير صحيحة.");
-}
+        if (!user) {
+            return res.status(401).send("البريد الإلكتروني أو كلمة المرور غير صحيحة.");
+        }
 
-const isMatch = await bcrypt.compare(password, user.password);
-if (!isMatch) {
-return res.status(401).send("البريد الإلكتروني أو كلمة المرور غير صحيحة.");
-}
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).send("البريد الإلكتروني أو كلمة المرور غير صحيحة.");
+        }
 
-const token = jwt.sign({ userId: user.userId, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ userId: user.userId, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-// الآن أضف userId هنا
-res.json({ token, role: user.role, userId: user.userId }); // أضف userId هنا
-} catch (error) {
-console.error("Error during login:", error);
-res.status(500).send("حدث خطأ أثناء محاولة تسجيل الدخول.");
-}
+        res.json({ token, role: user.role, userId: user.userId });
+    } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).send("حدث خطأ أثناء محاولة تسجيل الدخول.");
+    }
 });
-
 module.exports = router;

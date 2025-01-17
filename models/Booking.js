17367d1bedcf915178d0f2d14fea8b250e6dd369
tmrhan92 <63450846+tmrhan92@users.noreply.github.com>
2025-01-17 15:38:15 +0300

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const BookingSchema = new mongoose.Schema({
   userId: { type: String, required: true }, // استخدام userId بدلاً من therapistId
   serviceId: { type: String, ref: 'Service', required: true },
+  therapistId: { type: String, required: true },
   date: { type: Date, required: true },
   time: { type: String, required: true },
   status: { type: String, enum: ['pending', 'confirmed', 'cancelled'], default: 'pending' }

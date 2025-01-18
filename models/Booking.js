84222499ddb990e4d210c137859845e0d4046b06
tmrhan92 <63450846+tmrhan92@users.noreply.github.com>
2025-01-18 23:01:55 +0300

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -6,6 +6,10 @@ const BookingSchema = new mongoose.Schema({
   therapistId: { type: String, required: true },
   date: { type: Date, required: true },
   time: { type: String, required: true },
+    phone: { // إضافة حقل phone
+    type: String,
+    required: false // يمكن جعله مطلوباً إذا لزم الأمر
+  },
   status: { type: String, enum: ['pending', 'confirmed', 'cancelled'], default: 'pending' }
 }, { timestamps: true });
 

e2c1684f2bcecc80ed4c9cfce9ed3c8f0d356d5d
tmrhan92 <63450846+tmrhan92@users.noreply.github.com>
2025-01-19 16:19:41 +0300

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -9,7 +9,7 @@ const BookingSchema = new mongoose.Schema({
 userPhone: {
     type: String,
     required: true
-  } 
+  }, 
   status: { type: String, enum: ['pending', 'confirmed', 'cancelled'], default: 'pending' }
 }, { timestamps: true });
 

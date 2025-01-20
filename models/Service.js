4d617e3e81aba73368202fea14e39a0649106877
tmrhan92 <63450846+tmrhan92@users.noreply.github.com>
2025-01-20 16:25:58 +0300

    Update Service.js

--- models/Service.js
+++ models/Service.js
@@ -2,18 +2,20 @@
 const mongoose = require('mongoose');
 
 const ServiceSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  duration: { type: Number, required: true },
-  serviceType: { type: String, required: true },
-  subCategory: { type: String, required: true },
-  therapistId: { type: String, required: true },
-  latitude: { type: Number, required: true }, // خط العرض
-  longitude: { type: Number, required: true }, // خط الطول
-  governorate: { type: String, required: true }, // المحافظة
-  area: { type: String, required: true }, // المنطقة
-  createdAt: { type: Date, default: Date.now },
+_id: {
+type: String, // تعريف _id كسلسلة
+required: true,
+},
+name: { type: String, required: true },
+description: { type: String, required: true },
+serviceType: { type: String, required: true },
+price: { type: Number, required: true },
+duration: { type: Number, required: true },
+subCategory: { type: String, required: true }, // إضافة حقل الفئة الفرعية
+latitude: { type: Number, required: true },   // إحداثيات خط العرض
+longitude: { type: Number, required: true } ,   // إحداثيات خط الطول
+therapistId: { type: String,  required: true }, // _id للخدمة
+
 });
 
 

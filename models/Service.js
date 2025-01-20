f5a902eadd13db9db1cd9cb970bbafa9aca0c031
tmrhan92 <63450846+tmrhan92@users.noreply.github.com>
2025-01-20 19:57:46 +0300

    Update Service.js

--- models/Service.js
+++ models/Service.js
@@ -1,22 +1,24 @@
 // models/Service.js
 const mongoose = require('mongoose');
 
-const ServiceSchema = new mongoose.Schema({
-_id: {
-type: String, // تعريف _id كسلسلة
-required: true,
-},
-name: { type: String, required: true },
-description: { type: String, required: true },
-serviceType: { type: String, required: true },
-price: { type: Number, required: true },
-duration: { type: Number, required: true },
-subCategory: { type: String, required: true }, // إضافة حقل الفئة الفرعية
-latitude: { type: Number, required: true },   // إحداثيات خط العرض
-longitude: { type: Number, required: true } ,   // إحداثيات خط الطول
-therapistId: { type: String,  required: true }, // _id للخدمة
+const mongoose = require('mongoose');
 
+const serviceSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  serviceType: { type: String, required: true },
+  subCategory: { type: String, required: true },
+  price: { type: Number, required: true },
+  duration: { type: Number, required: true },
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
+  therapistId: { type: String, required: true },
+  province: { type: String, required: true }, // المحافظة
+  area: { type: String, required: true }, // المنطقة
 });
+
+module.exports = mongoose.model('Service', serviceSchema);
 
 
 module.exports = mongoose.model('Service', ServiceSchema);

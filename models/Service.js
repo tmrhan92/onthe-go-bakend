8dd2a185637d981a03527c609d531047b923b8f4
tmrhan92 <63450846+tmrhan92@users.noreply.github.com>
2025-01-21 21:43:18 +0300

    Update Service.js

--- models/Service.js
+++ models/Service.js
@@ -1,23 +1,67 @@
 const mongoose = require('mongoose');
 
-// تعريف Schema
 const ServiceSchema = new mongoose.Schema({
   _id: {
-type: String, // تعريف _id كسلسلة
-required: true,
-},
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  serviceType: { type: String, required: true },
-  subCategory: { type: String, required: true },
-  price: { type: Number, required: true },
-  duration: { type: Number, required: true },
-  latitude: { type: Number, required: true },
-  longitude: { type: Number, required: true },
-  therapistId: { type: String, required: true },
-  province: { type: String, required: true }, // المحافظة
-  area: { type: String, required: true }, // المنطقة
+    type: String, // تعريف _id كسلسلة
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  serviceType: {
+    type: String,
+    required: true,
+  },
+  subCategory: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  hoursRequired: {
+    type: Number,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+  therapistId: {
+    type: String,
+    required: true,
+  },
+  province: {
+    type: String,
+    required: true,
+  },
+  area: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['available', 'ongoing', 'completed'],
+    default: 'available',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-// تصدير النموذج
 module.exports = mongoose.model('Service', ServiceSchema);

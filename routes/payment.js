cb159d30b5543ea26fa0868da73f40e990a01b67
tmrhan92 <63450846+tmrhan92@users.noreply.github.com>
2025-02-15 00:26:59 +0300

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const auth = require('../middleware/auth'); // تأكد من المسار الصحيح
+const auth = require('./auth'); // تأكد من المسار الصحيح
 const User = require('../models/User');
 
 // إنشاء نية دفع

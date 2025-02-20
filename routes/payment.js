2f3ed92c90d5e1ee074715fd1b73712ac1e1bbae
tmrhan92 <63450846+tmrhan92@users.noreply.github.com>
2025-02-20 18:58:50 +0300

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const auth = require('../middleware/auth');
+const auth = require('./auth');
 const User = require('../models/User');
 
 // إنشاء نية دفع
